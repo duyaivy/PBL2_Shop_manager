c4af2d67c4f7985c6fdb9b67ad29a975aa5baa02
unknown <quocduy0322@gmail.com>
2024-10-19 07:39:37 +0700

    update code

--- main.cpp
+++ main.cpp
@@ -5,24 +5,19 @@ std::vector<person*> person::obj;
 
 int main() {
     person person1("John Doe", "123456789", "john@example.com", "password123","admin");
-    person person2("Jane Smith", "987654321", "jane@example.com", "password456", "customer");
+    person person2("Jane Smith", "987654321", "jane@example.com", "password456","customer");
 
-    
-//    // tao 1 nguoi moi va nhap thong tin
-//     person a;
-//     person::setInfor(a);
-//     a.setPass();
-//     person1.display();
-//     // delete vector nguoi
-//     person::clearPerson();
+
     person::display();
-    person z;
-    int login = person::login("0000001","password123", &z);
+    int login = person::login("0000001","password123", &p1);
     if(login){
-       if(z.getRole() == "admin") cout<< "admin";
+       if(p1.getRole() == "admin") cout<< "admin";
        else cout<< "cus";
+    }else{
+        cout<<"no";
     }
-    cout<<"may nhin cai cho gi"<<endl;
+   
+   
     
 
 
